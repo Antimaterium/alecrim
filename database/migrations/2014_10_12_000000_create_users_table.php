243dcd014070a83c0e1e3219a7bf9a571f7a59a5
ef-tadeu <edimar.tadeu.oliveira@gmail.com>
2017-08-25 07:54:09 -0300

    alterações dos itens

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email', 100)->unique();
             $table->string('password');
             $table->rememberToken();
+             $table->smallInteger('permission');
             $table->timestamps();
         });
     }
